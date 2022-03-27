12fc4775b58815a93d14c81bdff8995de7eac535
Amurfox <viktoriya.lisitsa@out.look.com>
2022-03-28 09:06:48 +1000

    new file

---
+++ JS/practice.js
@@ -0,0 +1,27 @@
+// 1. Declare a variable called age,  assign a value to age (for eg 25) and log it on the console.
+let age= 29;
+console.log(age);
+// 2. Declare a variable called myname, assign a value to myname and log it on the console.
+let myName= "Viktoriya";
+ console.log(myName);
+ // 3. Declare a constant greeting with a value of “Good day everyone”
+let  greeting = "Godd day everyone";
+console.log(greeting);
+// 4. Declare a boolean variable feelingGoodToday and assign a value to it.
+let feelingGoodToday = "true";
+console.log(feelingGoodToday); 
+// 5. Concatenate the values above from 1 to 4 to create a sentence like “Good day everyone. My name is Maya and I am 25 years old and my feel good factor is true/false”.
+let greet = greeting + "my Name is" + myName + "I am "+ age + "years old";
+console.log(greet);
+let newGreet = `${greeting} My name is ${myName} i am ${age} years old`;
+console.log(newGreet);
+// 6. Declare a variable lastname as undefined and log it on the console
+let lastname = "Lisitsa";
+console.log(lastname);
+// 7. Declare a variable address and assign a null value to it. Log it on the console
+let myAddress = null;
+console.log(myAddress);
+// 8. Use typeOf to check the data type of the variables above 
+typeOf(myAddress);
+// 9. Declare a variable x with a value of 10. Use the increment operation to increase its value by 1 and use decrement operator to decrease its value by 1.
+let x = "10";
